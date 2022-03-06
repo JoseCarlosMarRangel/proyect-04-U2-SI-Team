df01f61bf86f6d608b6b1fe0dfaa17dd08ec761c
JoseCarlosMarRangel <1930082@upv.edu.mx>
2022-03-05 22:49:06 -0600

    dataset detectado

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from skimage import io
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sn
+import pandas as pd
+import pickle
+import csv
+import tensorflow as tf
+from sklearn.metrics import confusion_matrix, classification_report
+import os
 
-img = io.imread('./dataset/BlackBishop(1).jpg')
-io.imshow(img)
-io.show()
+
+#img = io.imread('./dataset/BlackBishop(1).jpg')
+# io.imshow(img)
+# io.show()
+
+
+print(os.listdir('./dataset/'))
+path_image = './dataset/'
+image = io.imread(path_image + 'BlackBishop.jpg')
+imageplt = plt.imshow(image)
+plt.show()
+exit()
