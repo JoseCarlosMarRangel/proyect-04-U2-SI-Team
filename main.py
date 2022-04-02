16a9a06f1f911688284044bfa974a87b46b871fa
JoseCarlosMarRangel <1930082@upv.edu.mx>
2022-04-02 15:25:01 -0600

    Proyecto arreglado

--- main.py
+++ main.py
@@ -5,26 +5,25 @@ from skimage.morphology import closing
 from skimage.measure import label, regionprops, regionprops_table
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sn
 import pandas as pd
-import pickle
-import csv
 from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report
+from skimage.transform import resize
 from tqdm import tqdm
 import os
 
 
 # Prueba con una sola imagen
-image_path_list = os.listdir('./piezas/')
-i = 17
-image_path = image_path_list[i]
+image_path_list = os.listdir('./dataset/')
+i = 24
+image_path = 'tBlackTowerFour.jpg'
 # image = rgb2gray(io.imread('./dataset/'+image_path))
 
 # Aplica un filtro en la imagen para que este en blanco y negro
 # image_path = 'rey.jpg';
-image = rgb2gray(io.imread('./piezas/'+image_path))
+image = rgb2gray(io.imread('./dataset/tBlackTowerFour.jpg'))
+##image = resize(image,(480,640), anti_aliasing = True)
 imageplt = io.imshow(image)
 plt.show()
 
@@ -47,10 +46,28 @@ table = pd.DataFrame(regionprops_table(label_img, image,
                                         'major_axis_length',
                                         'minor_axis_length']))
 table['convex_ratio'] = table['area']/table['convex_area']
-table['label'] = image_path[5]
+std = []
+mean = []
+percent25 = []
+percent75 = []
+for prop in regionprops(label_img):
+
+    min_row, min_col, max_row, max_col = prop.bbox
+    img = image[min_row:max_row, min_col:max_col]
+    mean += [np.mean(img)]
+    std += [np.std(img)]
+    percent25 += [np.percentile(img, 25)]
+    percent75 += [np.percentile(img, 75)]
+table['mean_intensity'] = mean
+table['std_intensity'] = std
+table['25th Percentile'] = mean
+table['75th Percentile'] = std
+table['iqr'] = table['75th Percentile'] - table['25th Percentile']
+table['label'] = image_path[0]
 print("Tabla de los datos de la imagen:")
 print(table)
-
+imagetest = table.drop(['label'], axis = 1)
+imagetestlbl = table['label']
 # exit()
 
 # Prueba de implementacion de lo previo con todas las imagenes
@@ -62,7 +79,9 @@ df = pd.DataFrame()
 
 for i in range(len(image_path_list)):
     image_path = image_path_list[i]
+    print("Analizando imagen: " + image_path)
     image = rgb2gray(io.imread('./piezas/'+image_path))
+    ##image = resize(image,(640,480), anti_aliasing = True)
     binary = image < threshold_otsu(image)
     binary = closing(binary)
     label_img = label(binary)
@@ -94,27 +113,21 @@ for i in range(len(image_path_list)):
     table['25th Percentile'] = mean
     table['75th Percentile'] = std
     table['iqr'] = table['75th Percentile'] - table['25th Percentile']
-    table['label'] = image_path[5]
+    table['label'] = image_path[0]
     df = pd.concat([df, table], axis=0)
 
 df.head()
 print(df)
 
-X = df.drop(['label'], axis=1)
+X = df.drop(["label", "real_images"], axis=1)
+y = df["label"]
 
-# Features o características
-X = X[['iqr', 'real_images', 'mean_intensity',
-       'std_intensity', '25th Percentile', '75th Percentile']]
-
-
-# Targets
-y = df['label']
 columns = X.columns
 
 # train-test-split
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.25, random_state=123, stratify=y)
-
+##X_train, X_test, y_train, y_test = train_test_split(
+    ##X, y, test_size=0.25, random_state=123, stratify=y)
+X_train, X_test, y_train, y_test = X,X,y,y
 clf = GradientBoostingClassifier(
     n_estimators=50, max_depth=3, random_state=123)
 
@@ -122,7 +135,9 @@ clf.fit(X_train, y_train)  # print confusion matrix of test set
 # print accuracy score of the test set
 print(classification_report(clf.predict(X_test), y_test))
 final = {np.mean(clf.predict(X_test) ==
-                 y_test)*100: .2}
+                 y_test)*100}
 print("Test Accuracy: " + str(final) + "%")
-
-exit()
+#from collections import Counter
+#mylist = clf.predict(imagetest)
+#c = Counter(mylist)
+#print(c.most_common(1), imagetestlbl.iloc[0])
